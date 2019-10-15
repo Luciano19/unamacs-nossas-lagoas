a7c0799d336f44f1731126afc40dc5336b6c72ac
Davy de Matos <davymatos01@gmail.com>
2019-10-14 23:50:47 -0300

    Modificação na view

--- nossas-lagoas/resources/views/cadastro_lagoas.blade.php
+++ nossas-lagoas/resources/views/cadastro_lagoas.blade.php
@@ -6,17 +6,18 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <title>Cadastro de Lagoas</title>
     </head>
-    @extends(["current"=>"lagoas"])
-    </head>
+    
     <body>
-        <div class="container col-10">
-            <form action = "{{route('lagoas.store')}}" method = "POST">
-            @csrf
-                <h1>Davy</h1>
+        @extends('cadastro_lagoas',["current"=>"lagoas"])
+        <form action = "{{route('lagoas.store')}}" method = "POST">
+        @csrf
+            <div class="container col-10">
+            
                 <label for="nome">Nome</label>
-                <input  type = "text" class = "form-control" id="nome" name="nome" placeholder="" value="" required>
-            </form>
-        </div>
+                <input  type = "text" class = "form-control" id="nome" name="nome">
+            </div>
+        </form>
+
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+9655DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
